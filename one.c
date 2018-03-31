5dfdace98dd3dedf1f2ea52d6f6bfd76525e5375
Lung-Chen Huang <perfecthuang413@gmail.com>
2018-03-31 20:46:00 +0800

    modify module code

--- one.c
+++ one.c
@@ -6,7 +6,7 @@
 #include <linux/types.h>
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 #define MAJOR_NUMBER 61
 
@@ -37,15 +37,29 @@ int onebyte_release(struct inode *inode, struct file *filep)
 {
     return 0; // always successful
 }
-ssize_t onebyte_read(struct file *filep, char *buf, size_t
-count, loff_t *f_pos)
+ssize_t onebyte_read(struct file *filep, char *buf,
+size_t count, loff_t *f_pos)
 {
-    /*please complete the function on your own*/
+    if (*f_pos) {
+        return 0;
+    }
+    if (copy_to_user(buf, onebyte_data, 1)) {
+        return -EFAULT;
+    }
+    *f_pos += 1;
+    return count;
 }
 ssize_t onebyte_write(struct file *filep, const char *buf,
 size_t count, loff_t *f_pos)
 {
-    /*please complete the function on your own*/
+    if (copy_from_user(onebyte_data, buf, 1)) {
+        return -EFAULT;
+    }
+    if (count > 1) {
+        return -ENOSPC;
+    }
+
+    return count;
 }
 
 static int onebyte_init(void)
@@ -64,8 +78,7 @@ static int onebyte_init(void)
     if (!onebyte_data) {
         onebyte_exit();
         // cannot allocate memory
-        // return no memory error, negative signify a
-        failure
+        // return no memory error, negative signify a failure
         return -ENOMEM;
     }
     // initialize the value to be X
